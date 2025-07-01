271c1fcb4bde155d20303f44a68db9795b578586
raghx <agarwal1996araghav@gmail.com>
2025-07-01 08:17:57 +0530

    Nqueens problem solved

--- src/BackTracking/Nqueens.java
+++ src/BackTracking/Nqueens.java
@@ -3,15 +3,50 @@ package BackTracking;
 
 public class Nqueens {
     public static void main(String[] args) {
-        int n = 4;
-        boolean[][] maze = new boolean[n][n];
+        int n = 6;
+        boolean[][] maze = new boolean[n][n];    //false values
+        System.out.print(NQueens(0,maze));
+
+
     }
-    public static void NQueens(int row,int column,boolean[][] maze ){
-        if(row == maze.length ){
+    public static int NQueens(int row,boolean[][] maze ){
+        int count = 0;
+        if(row == maze.length){
             display(maze);
-            return ;
+            System.out.println();
+            return 1;
         }
-        maze[row][column] =
+        for(int j = 0 ; j < maze.length ; j++){
+            if(issafe(row,j,maze)){
+                maze[row][j] = true;
+                count = count+NQueens(row+1,maze);      //if safe , move to next row  , if not move to next column in called row
+                maze[row][j] = false;         // backtrack
+            }
+        }
+        return count;
+    }
+
+    private static boolean issafe(int row, int col, boolean[][] maze) {
+        int n = maze.length-1;
+        for(int i = 0 ; i < row ; i++){
+            if(maze[i][col]){
+                return false;
+            }
+        }
+        int maxLeft = Math.min(row,col);
+        for(int i = 1 ; i <= maxLeft ; i++){
+            if(maze[row-i][col-i]){
+                return false;
+            }
+        }
+        int maxRight = Math.min(row,n-col);
+        for(int i = 1 ; i <= maxRight ; i++){
+            if(maze[row-i][col+i]){
+                return false;
+            }
+        }
+return true;
+
     }
 
     private static void display(boolean[][] maze) {
@@ -21,10 +56,11 @@ public class Nqueens {
                     System.out.print("Q");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print("X");
                 }
             }
             System.out.println();
+
         }
     }
 }
