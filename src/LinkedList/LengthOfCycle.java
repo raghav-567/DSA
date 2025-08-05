d57a50b220d127dfbdb1cb1f414e0669d86d9c8c
raghav-567 <agarwal1996raghav@gmail.com>
2025-08-05 22:39:27 +0530

    added length of cycle calculation

---
+++ src/LinkedList/LengthOfCycle.java
@@ -0,0 +1,25 @@
+package LinkedList;
+public class LengthOfCycle {
+    public static void main(String[] args) {
+
+//        ListNode Fast = head;
+//        ListNode Slow = head;
+//        int length = 0;
+//        while(Fast != null && Fast.next != null){
+//            Fast = Fast.next.next;
+//            Slow = Slow.next;
+//            if(Fast == Slow){
+//                ListNode temp = Slow;
+//                Slow = Slow.next;
+//                 length += 1;
+//                while(Slow != temp){
+//                    Slow = Slow.next;
+//                    length++;
+//                }
+//            }
+//        }
+//return length;
+
+    }
+
+}
