fd9d566cb31e1c85486b228c7c59350d2399581e
raghav-567 <agarwal1996raghav@gmail.com>
2025-08-06 22:29:48 +0530

    HeadOfCycle(leetcode) added

---
+++ src/LinkedList/HeadOfCycle.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class HeadOfCycle {
+//    public static void main(String[] args) {
+//
+//    }
+//    public ListNode detectCycle(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        while(fast != null && fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if(slow == fast){
+//                ListNode temp = head;
+//                while(slow != temp){
+//                    slow = slow.next;
+//                    temp = temp.next;
+//                }
+//                return temp;
+//            }
+//        }
+//
+//        return null;
+//    }
+}
