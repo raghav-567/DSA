078e823271034964f4a51f15bf4d59424dc9ddc6
raghav-567 <agarwal1996raghav@gmail.com>
2025-08-07 22:42:23 +0530

    MiddleofLL(leetcode)

---
+++ src/LinkedList/MiddleOfLL.java
@@ -0,0 +1,38 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MiddleOfLL {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        MiddleOfLL obj = new MiddleOfLL();
+        ListNode middle = obj.middleNode(head);
+
+        System.out.println("Middle Node Value: " + middle.val); // Output: 3
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+
