353133a17dd8c71c4472c44135ce0cc7de321206
raghav-567 <agarwal1996raghav@gmail.com>
2025-08-14 23:52:42 +0530

    modified classes structure

--- src/LinkedList/MiddleOfLL.java
+++ src/LinkedList/MiddleOfLL.java
@@ -1,15 +1,5 @@
 package LinkedList;
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
 public class MiddleOfLL {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
