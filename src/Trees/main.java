82248082c2ca01b0c42866d65e1adb237994f14c
raghav-567 <agarwal1996raghav@gmail.com>
2025-09-08 09:23:49 +0530

    Added BinaryTree Implementation

--- src/Trees/main.java
+++ src/Trees/main.java
@@ -1,4 +1,12 @@
 package Trees;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+         tree.populate(scanner);
+         tree.prettyDisplay();
+    }
 }
