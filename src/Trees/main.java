55ffaaacecde59ea4017f2bca78e5ac559b165dc
raghav-567 <agarwal1996raghav@gmail.com>
2025-09-10 22:39:18 +0530

    Added BST implementation

--- src/Trees/main.java
+++ src/Trees/main.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-         tree.populate(scanner);
-         tree.prettyDisplay();
+//         Scanner scanner = new Scanner(System.in);
+//        BinaryTree tree = new BinaryTree();
+//         tree.populate(scanner);
+//         tree.prettyDisplay();
+
+        BST tree = new BST();
+        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+        tree.populate(nums);
+        tree.display();
     }
 }
