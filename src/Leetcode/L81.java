d973b36e6c23db6dee8b4d55b8fff7372e9b7b51
raghav-567 <agarwal1996raghav@gmail.com>
2025-10-05 23:10:19 +0530

    Solved leetcode 81

---
+++ src/Leetcode/L81.java
@@ -0,0 +1,16 @@
+package Leetcode;
+
+public class L81 {
+    public static void main(String[] args) {
+
+    }
+    public boolean search(int[] nums, int target) {
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == target )
+            {
+                return true;
+            }
+        }
+        return false ;
+    }
+}
