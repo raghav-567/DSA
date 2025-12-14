107395c7b53808c6bdbed8b90d48903a0d6b35b9
raghav-567 <agarwal1996raghav@gmail.com>
2025-12-14 22:06:56 +0530

    Solved leetcode contest problem

--- src/Leetcode_contest/Weekly480_2.java
+++ src/Leetcode_contest/Weekly480_2.java
@@ -1,4 +1,29 @@
 package Leetcode_contest;
 
 public class Weekly480_2 {
+    public static void main(String[] args) {
+
+    }
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        int targetVowels = countVowels(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            if (countVowels(words[i]) == targetVowels) {
+                words[i] = new StringBuilder(words[i]).reverse().toString();
+            }
+        }
+
+        return String.join(" ", words);
+    }
+
+    private int countVowels(String word) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
 }
