83d184e4722481ac9375673160ec92dbefecaf9d
raghav-567 <agarwal1996raghav@gmail.com>
2025-12-14 22:04:18 +0530

    Solved leetcode contest problem

--- src/Leetcode_contest/Weekly480_1.java
+++ src/Leetcode_contest/Weekly480_1.java
@@ -1,4 +1,22 @@
 package Leetcode_contest;
 
+import java.util.Arrays;
+
 public class Weekly480_1 {
+    public static void main(String[] args) {
+
+    }
+    public int absDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int smallSum = 0;
+        int largeSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            smallSum += nums[i];
+            largeSum += nums[nums.length - 1 - i];
+        }
+
+        return Math.abs(largeSum - smallSum);
+    }
 }
