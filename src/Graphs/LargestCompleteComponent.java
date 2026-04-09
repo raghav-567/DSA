62f7a213df42b890bac1fcaa9f8c19d927cb9bc9
raghav-567 <agarwal1996raghav@gmail.com>
2026-04-09 23:53:01 +0530

    solved graph problem

--- src/Graphs/LargestCompleteComponent.java
+++ src/Graphs/LargestCompleteComponent.java
@@ -1,4 +1,36 @@
 package Graphs;
+import java.util.*;
 
 public class LargestCompleteComponent {
+    public static void main(String[] args) {
+//        Write a method, largestComponent, that takes in the adjacency list of an undirected graph. The method should return the size of the largest connected component in the graph.
+    }
+
+    public static int largestComponent(Map<Integer, List<Integer>> graph) {
+        int count = 0;
+        int max = 0;
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (int node : graph.keySet()) {
+            count = dfs(node, visited, graph);
+            if (count > max) max = count;
+        }
+
+        return max;
+    }
+
+    public static int dfs(int node, HashSet<Integer> visited, Map<Integer, List<Integer>> graph) {
+        if (visited.contains(node)) {
+            return 0;
+        }
+        visited.add(node);
+
+        int count = 1;
+        for (int nei : graph.get(node)) {
+
+            count += dfs(nei, visited, graph);
+
+        }
+        return count;
+    }
 }
