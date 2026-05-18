ad03dbe241a0fb4674356a674143a571783ca8fa
raghav-567 <agarwal1996raghav@gmail.com>
2026-05-18 23:38:54 +0530

    Solved headp question

---
+++ src/Heap/L215.java
@@ -0,0 +1,25 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class L215 {
+    public static void main(String[] args) {
+
+    }
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+
+        return minHeap.peek();
+
+    }
+}
